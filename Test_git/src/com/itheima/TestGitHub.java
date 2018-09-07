18476cc1313a61abd119a935a2a677090f7a5990
zhangsan <zhangsan@itcast.com>
2018-09-07 20:48:00 +0800

    添加方法

---
+++ Test_git/src/com/itheima/TestGitHub.java
@@ -0,0 +1,9 @@
+package com.itheima;
+
+public class TestGitHub {
+    public static void main(String[] args) {
+        System.out.println("hahaahhaha");
+        System.out.println("hahaahhaha");
+        System.out.println("hahaahhaha");
+    }
+}
